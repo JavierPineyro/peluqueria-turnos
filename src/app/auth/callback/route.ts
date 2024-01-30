1f00a61edb027876d6e5f4c0cba1eaab93de9c69
JavierPineyro <javidunam22@gmail.com>
2024-01-30 18:42:43 -0300

    add login supabase feature

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,14 +1,15 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-import { NextRequest, NextResponse } from 'next/server'
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
-    const requestUrl = new URL(req.url)
-    const code = requestUrl.searchParams.get('code')
+  const requestUrl = new URL(req.url);
+  const code = requestUrl.searchParams.get("code");
 
-    if (code) {
-        const supabase = createRouteHandlerClient({ cookies })
-        await supabase.auth.exchangeCodeForSession(code)
-    }
-    return NextResponse.redirect(requestUrl.origin)
+  if (code) {
+    const supabase = createRouteHandlerClient({ cookies });
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+  return NextResponse.redirect(requestUrl.origin);
 }
