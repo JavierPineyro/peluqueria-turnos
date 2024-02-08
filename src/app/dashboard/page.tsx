40df7c842ca03cf684349906c09c2a3f3c026d59
JavierPineyro <javidunam22@gmail.com>
2024-02-08 16:54:03 -0300

    Add filter for dates in calendar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,13 +3,27 @@ import CrearTurno from "@/components/crear-turno";
 import { Events, Service } from "@/lib/types";
 import { mapEventResponse } from "@/lib/utils";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import dayjs from "dayjs";
 import { cookies } from "next/headers";
 
-export default async function DashboardPage() {
+export default async function DashboardPage(
+  { searchParams }: { searchParams: { days: number } }
+) {
+
+  // Usado para que no se muestren los turnos anteriores a 60 dias.
+  const DAYS = Number(searchParams.days || "60")
+  const minDate = dayjs().subtract(DAYS, "day").format()
 
   const supabase = createServerComponentClient({ cookies })
-  const { data } = await supabase.from("turnos").select("*, services(*)") as { data: Events }
-  const { data: services } = await supabase.from("services").select("*") as { data: Service[] }
+  const { data } = await supabase
+    .from("turnos")
+    .select("*, services(*)")
+    .gt("start", minDate) as { data: Events }
+
+  const { data: services } = await supabase
+    .from("services")
+    .select("*").order("price", { ascending: true }) as { data: Service[] }
+
 
   const events = mapEventResponse(data)
 
