581827b4f5b1c3cd4994e0dfd60968d600b6c744
JavierPineyro <javidunam22@gmail.com>
2024-02-09 15:28:08 -0300

    Fix error page styles bug

--- src/app/dashboard/error.tsx
+++ src/app/dashboard/error.tsx
@@ -6,8 +6,8 @@ import Link from "next/link"
 export default function ErrorPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-4 bg-gradient-to-br from-[#2e026d] to-[#15162c] text-white">
-      <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-        Something went wrong
+      <h1 className="text-3xl font-extrabold tracking-tight sm:text-[5rem]">
+        Algo salió Mal
       </h1>
       <Link
         className="text-xl font-bold underline underline-offset-4 hover:text-blue-500 transition-colors duration-200"
