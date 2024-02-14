5bfe1c9df6f838fbfc9c135b68ba755110eb9265
JavierPineyro <javidunam22@gmail.com>
2024-02-14 15:10:37 -0300

    Add sonner dependency to show results on toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Lato } from 'next/font/google'
 import './globals.css'
+import { Toaster } from '@/components/ui/sonner'
 
 const latoFont = Lato({
   subsets: ['latin'], variable: '--font-lato', weight: ['400', '700'], display: 'swap', preload: true, adjustFontFallback: true, style: 'normal', fallback: ['Roboto', 'Arial', 'sans-serif']
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="es">
       <body className={`${latoFont.className} bg-black dark`}>
         {children}
+        <Toaster closeButton richColors position="top-center" />
       </body>
     </html>
   )
