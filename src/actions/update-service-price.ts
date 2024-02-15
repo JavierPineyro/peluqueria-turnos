755c7cf4de0eb6a452cfa36e1199800b8acb08bf
JavierPineyro <javidunam22@gmail.com>
2024-02-15 18:06:31 -0300

    refactor server acction with try catch statement

--- src/actions/update-service-price.ts
+++ src/actions/update-service-price.ts
@@ -1,30 +1,41 @@
 "use server";
 
+import { getErrorMessage } from "@/lib/utils";
 import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
 import { revalidatePath } from "next/cache";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function updatePrice(id: string, formData: FormData) {
-  const nameData = formData.get("name") as string;
-  const priceData = Number(formData.get("price"));
+  try {
+    const nameData = formData.get("name") as string;
+    const priceData = Number(formData.get("price"));
 
-  if (!nameData || !priceData) {
-    throw new Error("Invalid inputs at updatePrice action");
-  }
+    if (!nameData || !priceData) {
+      throw new Error("Datos inválidos para actualizar");
+    }
 
-  const services = {
-    id: id.toString(),
-    name: nameData.toString(),
-    price: priceData,
-  };
+    const services = {
+      id: id.toString(),
+      name: nameData.toString(),
+      price: priceData,
+    };
 
-  const supabase = createServerActionClient({ cookies });
-  const res = await supabase.from("services").update(services).eq("id", id);
+    const supabase = createServerActionClient({ cookies });
+    const res = await supabase.from("services").update(services).eq("id", id);
 
-  if (res.status === 204) {
+    if (res.status !== 204) {
+      throw new Error(
+        "Error actualizando el servicio, inténtelo de nuevo más tarde"
+      );
+    }
+
     revalidatePath("/dashboard/precios");
-    redirect("/dashboard/precios");
+    // redirect("/dashboard/precios");
+  } catch (error) {
+    console.error(error);
+    return {
+      message: getErrorMessage(error),
+    };
   }
-  throw new Error("Error updating service at updatePrice action, server error");
 }
