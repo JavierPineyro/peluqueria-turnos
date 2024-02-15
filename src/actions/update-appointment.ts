47265663e27352f0fb2b71b833409cc829ac54f1
JavierPineyro <javidunam22@gmail.com>
2024-02-15 17:30:21 -0300

    Add handling error on server action

--- src/actions/update-appointment.ts
+++ src/actions/update-appointment.ts
@@ -7,39 +7,45 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function updateAppointment(id: string, formData: FormData) {
-  let titleData = formData.get("title");
-  let startData = formData.get("start") as string;
-  let endData = formData.get("end") as string;
-  let serviceData = formData.get("service");
-  let statusData = formData.get("status");
+  try {
+    let titleData = formData.get("title");
+    let startData = formData.get("start") as string;
+    let endData = formData.get("end") as string;
+    let serviceData = formData.get("service");
+    let statusData = formData.get("status");
 
-  if (
-    !id ||
-    !titleData ||
-    !statusData ||
-    !startData ||
-    !endData ||
-    !serviceData
-  ) {
-    throw new Error("Missing appointment data at updateAppointment");
-  }
+    if (
+      !id ||
+      !titleData ||
+      !statusData ||
+      !startData ||
+      !endData ||
+      !serviceData
+    ) {
+      throw new Error("Faltan datos del turno para actualizar");
+    }
 
-  const appointment = {
-    title: titleData.toString(),
-    start: dayjs(startData).format(),
-    status: statusData.toString(),
-    end: dayjs(endData).format(),
-    service: serviceData.toString(),
-  };
+    const appointment = {
+      title: titleData.toString(),
+      start: dayjs(startData).format(),
+      status: statusData.toString(),
+      end: dayjs(endData).format(),
+      service: serviceData.toString(),
+    };
 
-  const supabase = createServerActionClient({ cookies });
-  const res = await supabase.from("turnos").update(appointment).eq("id", id);
+    const supabase = createServerActionClient({ cookies });
+    const res = await supabase.from("turnos").update(appointment).eq("id", id);
 
-  if (res.status === 204) {
+    if (res.status !== 204) {
+      throw new Error("Error actualizando el turno, inténtalo más tarde");
+    }
+
     revalidatePath("/dashboard");
-    redirect("/dashboard");
+    // redirect("/dashboard");
+  } catch (error) {
+    console.error(error);
+    return {
+      message: error.message,
+    };
   }
-  throw new Error(
-    "Error creating new appointment at updateAppointment action, server error"
-  );
 }
